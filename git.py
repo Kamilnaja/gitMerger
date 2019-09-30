7a9e13771d9b5ea87b012ecea5db13fdd51dee10
najak <kamilnaja@gmail.com>
2019-09-30 21:12:46 +0200

    wip

--- git.py
+++ git.py
@@ -1,14 +1,25 @@
+import subprocess
+initialBranch = "pex"
+branchToCheckout = "0000"
 ok = 'Already up to date'
 cannotPull = 'cannot pull with rebase: You have unstaged changes'
-import subprocess
-output = subprocess.check_output(["git", "pull", "--rebase"])
 
 def utf8Decode(input):
   return input.decode('utf-8')
 
-if ok in utf8Decode(output):
-  print(ok)
-elif cannotPull in utf8Decode(output):
-  print('Need stash or commit')
-else:
-  print(utf8Decode(output))
+try:
+    output = subprocess.check_output(["git", "pull", "--rebase"])
+except subprocess.CalledProcessError as e:
+  print(e.output)
+  pull = subprocess.check_output(["git", "stash"])
+  print("stashing changes")
+  # if cannotPull in utf8Decode(e.output):
+
+# if ok in utf8Decode(output):
+#   print(ok)
+#   co = subprocess.check_output(["git", branchToCheckout])
+
+# elif cannotPull in utf8Decode(output):
+#   print('Need stash or commit')
+# else:
+#   print(utf8Decode(output))
